e71bbfa84bb0bd1ea770f35bf91d86f99f549e15
FarisAsh <FarisTheCrafter@gmail.com>
2024-11-07 02:09:29 -0800

    Simple Sums

---
+++ Exercises/Exercise 2 Simple Sums/Exercise 2 Simple Sums.py
@@ -0,0 +1,4 @@
+Eight=8#assigning an integer to the variable A
+Ten=10#assigning an integer to the variable B
+Eighteen=Eight+Ten#assigning the sum of the two variables to a third variable
+print(Eighteen)#Displaying the sum
