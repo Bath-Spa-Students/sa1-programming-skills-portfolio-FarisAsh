057ab0da93a6dc1cde553dd6678d176f2dbb373b
FarisAsh <FarisTheCrafter@gmail.com>
2024-11-20 22:03:38 -0800

    Hello

---
+++ Exercises/Exercise 9 Hello/Hello.py
@@ -0,0 +1,6 @@
+def hello():
+    print("Hello")  #output hello as message to the computer
+def main():
+    hello()  #it calls the function "hello()" to execute it
+if __name__ == "__main__": #checking if script is run directly from main
+    main()  #it calls "main():" function, if the script is running directly from main
